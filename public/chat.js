37ef917216eee8f3f047a6818c70d559658436e4
SandroJažo <sjazo00@fesb.hr>
2021-11-08 15:16:04 +0100

    initial commit

---
+++ public/chat.js
@@ -0,0 +1,128 @@
+var socket = io.connect("http://localhost:4000");
+var divVideoChatLobby = document.getElementById("video-chat-lobby");
+var divVideoChat = document.getElementById("video-chat-room");
+var joinButton = document.getElementById("join");
+var userVideo = document.getElementById("user-video");
+var peerVideo = document.getElementById("peer-video");
+var roomInput = document.getElementById("roomName");
+var roomName = roomInput.value;
+
+var creator = false;
+var rtcPeerConnection;
+var userStream;
+
+var iceServers = {
+    iceServers:[
+        {urls: "stun:stun.l.google.com:19302"},
+        {urls: "stun:stun.services.mozilla.com"}
+    ]
+}
+
+joinButton.addEventListener('click', function(){
+    if(roomInput.value == ''){
+        alert("Please enter a room name");
+    }
+    else {
+        socket.emit("join", roomName);
+    }
+})
+
+socket.on("created", function(){
+    creator = true;
+    navigator.getUserMedia({
+            audio: true,
+            video: {width: 1280, height: 720}
+        }, function(stream){
+            userStream = stream;
+            divVideoChatLobby.style = "display:none";
+            userVideo.srcObject = stream;
+            userVideo.onloadedmetadata = function(e) {
+                userVideo.play();
+            };
+        },
+        function (){
+            alert("Couldn't Access User Media");
+        });
+});
+
+socket.on("joined", function(){
+    creator = false;
+    navigator.getUserMedia({
+            audio: true,
+            video: {width: 1280, height: 720}
+        }, function(stream){
+            userStream = stream;
+            divVideoChatLobby.style = "display:none";
+            userVideo.srcObject = stream;
+            userVideo.onloadedmetadata = function(e) {
+                userVideo.play();
+            };
+            socket.emit("ready", roomName);
+        },
+        function (){
+            alert("Couldn't Access User Media");
+        });
+});
+
+socket.on("full", function(){
+    alert("Room is Full, Can't Join");
+});
+
+socket.on("ready", function(){
+    if(creator){
+        rtcPeerConnection = new RTCPeerConnection(iceServers);
+        rtcPeerConnection.onicecandidate = OnIceCandidateFunction;
+        rtcPeerConnection.ontrack = OnTrackFunction;
+        rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
+        rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
+        rtcPeerConnection.createOffer(function(offer){
+            rtcPeerConnection.setLocalDescription(offer);
+            socket.emit("offer", offer, roomName);
+        }, function(error){
+            console.log(error);
+        })
+    }
+});
+
+socket.on("candidate", function(candidate){
+    var icecandidate = new RTCIceCandidate({
+        candidate: candidate.candidate,
+        sdpMid: candidate.sdpMid,
+        sdpMLineIndex: candidate.sdpMLineIndex
+    });
+    rtcPeerConnection.addIceCandidate(icecandidate);
+});
+
+socket.on("offer", function(offer){
+    if(!creator){
+        rtcPeerConnection = new RTCPeerConnection(iceServers);
+        rtcPeerConnection.onicecandidate = OnIceCandidateFunction;
+        rtcPeerConnection.ontrack = OnTrackFunction;
+        rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
+        rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
+        rtcPeerConnection.setRemoteDescription(offer);
+        rtcPeerConnection.createAnswer(function(answer){
+            rtcPeerConnection.setLocalDescription(answer);
+            socket.emit("answer", answer, roomName);
+        }, function(error){
+            console.log(error);
+        })
+    }
+});
+
+socket.on("answer", function(answer){
+    rtcPeerConnection.setRemoteDescription(answer);
+});
+
+function OnIceCandidateFunction(event){
+    if(event.candidate){
+        socket.emit("candidate", event.candidate, roomName);
+    }
+}
+
+function OnTrackFunction(event){
+    peerVideo.srcObject = event.streams[0];
+    peerVideo.onloadedmetadata = function(e) {
+        peerVideo.play();
+    };
+}
